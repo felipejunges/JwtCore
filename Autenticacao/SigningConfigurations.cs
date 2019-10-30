5dc3d3068858bad213c0d8c93f394ce7918a1b49
Felipe Junges <felipejunges@outlook.com>
2019-10-29 22:17:48 -0300

    :ok_hand: Removidos comentários

--- Autenticacao/SigningConfigurations.cs
+++ Autenticacao/SigningConfigurations.cs
@@ -10,12 +10,6 @@ namespace jwtcore.Autenticacao
 
         public SigningConfigurations(string keyString)
         {
-            // using (var provider = new RSACryptoServiceProvider(2048))
-            // {
-            //     Key = new RsaSecurityKey(provider.ExportParameters(true));
-            // }
-            // SigningCredentials = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature);
-
             Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
             
             SigningCredentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
