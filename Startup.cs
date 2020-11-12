22e51513deee6a098582baf979e61aa61d84add7
Felipe Amadeu Junges <felipejunges@outlook.com>
2020-11-11 21:41:10 -0300

    :sparkles: Swagger

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace jwtcore
 {
@@ -26,6 +31,52 @@ namespace jwtcore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "JwtCore",
+                        Version = "v1",
+                        Description = "Exemplo de API REST para teste de autorização via JWT.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Felipe Junges",
+                            Url = new Uri("https://www.mangos.inf.br")
+                        }
+                    });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. <br /> 
+                      Enter 'Bearer' [space] and then your token in the text input below. <br />
+                                  Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            //
             var tokenConfigurations = new TokenConfigurations();
             new ConfigureFromConfigurationOptions<TokenConfigurations>(
                 Configuration.GetSection("TokenConfigurations"))
@@ -104,6 +155,12 @@ namespace jwtcore
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JwtCore");
+            });
 
             app.UseEndpoints(endpoints =>
             {
